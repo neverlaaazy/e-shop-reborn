840286bbb273560ab3e5c9b4de1d6b216bf53951
neverlaaazy <cheserega174@gmail.com>
2026-05-18 18:46:52 +0500

    27 small change

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -5,7 +5,7 @@
     >
     <div class="container mx-auto">
         <h2>{{$product->title}}</h2>
-        <div class="border mb-4 flex">
+        <div class="border mb-4 flex showcard">
             @if($product->image_url)
                 <img class="w-28 h-full aspect-1 mr-4" src="{{ $product->image_url }}"
                     alt="{{ $product->title }}">
@@ -14,7 +14,9 @@
                 <p>{{ $product->description }}</p>
                 <p>Цена: {{ $product->price}}</p>
                 <p>Категория: {{ $product->category->title }}</p>
+                <p>Количество: {{ $product->count }}</p>
             </div>
+            <button>Купить</button>
         </div>
     </div>
 </x-main-layout>
